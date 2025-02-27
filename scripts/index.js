bfa5616f5f3d34590d1f1ad44dc3140100ec6694
amberferger <aferger1@gmail.com>
2025-02-27 16:02:27 -0500

    Add javascript file

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Banff",
+    link: "https://unsplash.com/photos/scenery-of-mountain-oMneOBYhJxY",
+  },
+  {
+    name: "Lisbon",
+    link: "https://unsplash.com/photos/yellow-and-white-tram-on-road-during-daytime-ljhCEaHYWJ8",
+  },
+  {
+    name: "London",
+    link: "https://unsplash.com/photos/aerial-photography-of-london-skyline-during-daytime-Oja2ty_9ZLM",
+  },
+  {
+    name: "Dublin",
+    link: "https://unsplash.com/photos/brown-and-white-concrete-houses-tnzzr8HpLhs",
+  },
+  {
+    name: "Munich",
+    link: "https://unsplash.com/photos/aerial-view-of-city-buildings-during-sunset-MEUvVqkU3QI",
+  },
+  {
+    name: "Edinburgh",
+    link: "https://unsplash.com/photos/brown-concrete-building-near-river-during-daytime-TmDEY0DtQd0",
+  },
+];
