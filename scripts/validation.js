999be608d1caa863477877755d00260e4e7639f9
amberferger <aferger1@gmail.com>
2025-04-06 20:10:41 -0400

    feature: add error validation message and disable submit button if invalid input

---
+++ scripts/validation.js
@@ -0,0 +1,65 @@
+// displays input error
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  errorElement.textContent = errorMessage; // changes error message
+  inputElement.classList.add("modal__input_type_error"); // styles the box when error occurs
+};
+
+// hides input error
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  errorElement.textContent = ""; // removes error message
+  inputElement.classList.remove("modal__input_type_error");
+};
+
+// checks validity of input element
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+// checks to see if any input is invalid
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+// disables button if 1+ input is invalid
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true; // disable button if 1+ invalid inputs
+    buttonElement.classList.add("modal__submit-button_type_inactive");
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove("modal__submit-button_type_inactive");
+  }
+};
+
+// set the event listeners on the inputs
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
+  const buttonElement = formElement.querySelector(".modal__submit-button");
+
+  //toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+// adding validation to each form
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".modal__form"));
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
